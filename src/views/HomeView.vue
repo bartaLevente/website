6b2c0062985ea9b833cc43bca43aa895765379fb
smokk <pdc.levike@gmail.com>
2025-01-07 17:28:48 +0100

    landing page demo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-    Hello there, general Kenobi
+    <div class="wrapper">
+      <div class="picture">
+        <img src="@/assets/img/images/cv_pic.jpg" alt="My CV picture">
+        <div class="border topleft"></div>
+        <div class="border botright"></div>
+      </div>
+      <div class="intro-text">
+        <h1>Barta Levente</h1>
+        <h2>Welcome to my portfolio! I’m a passionate software developer with a focus on creating innovative, efficient solutions through clean code and modern technologies. With experience across web development, deep learning, and AI projects, I’m constantly seeking to push the boundaries of what’s possible. Let's get started!</h2>
+      </div>
+    </div>
   </div>
 </template>
 
